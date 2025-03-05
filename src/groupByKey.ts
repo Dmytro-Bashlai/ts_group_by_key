22392e98b60688f8c5325fe53a2ff961b133b79b
Dmytro Bashlai <imaxs2016@gmail.com>
2025-03-05 23:58:34 +0200

    Several issues have been fixed

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,11 +2,16 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey<T>(items: T[], key: keyof T[]): GroupsMap<T> {
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   return items.reduce((groups, item) => {
-    return {
-      ...groups,
-      [item[key]]: items.filter((filterItem) => item[key] === filterItem[key]),
-    };
+    const keyValue = String(item[key]);
+
+    if (!groups[keyValue]) {
+      groups[keyValue] = [];
+    }
+
+    groups[keyValue].push(item);
+
+    return groups;
   }, {});
 }
